a468f694947bf96aaf267ad1025c256c48fdddc4
Jayden Galea <jaydengalea11@gmail.com>
2025-08-07 19:06:53 +1000

    Set up routing and Redux store structure

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import postsReducer from './features/posts/postsSlice'
+import filtersReducer from './features/filters/filterSlice'
+import commentsReducer from './features/comments/commentsSlice'
+
+const store = configureStore({
+  reducer: {
+    posts: postsReducer,
+    filters: filtersReducer,
+    comments: commentsReducer,
+  },
+})
+
+export default store
